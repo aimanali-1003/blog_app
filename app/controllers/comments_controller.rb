81ab31b272d7790258f0270766619a91c9fed5e3
aimanali-1003 <aiman.ali@devsinc.com>
2022-11-01 17:03:39 +0500

    DRY code

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,18 +1,26 @@
 class CommentsController < ApplicationController
+
+    before_action :set_article, only: %i[create destroy ]
+    before_action :set_comment, only: %i[destroy]
     def create
-        @article = Article.find(params[:article_id])
-        @Comment = @article.comments.create(comment_params)
+        @comment = @article.comments.create(comment_params)
         redirect_to article_path(@article)
     end
 
     def destroy
-        @article = Article.find(params[:article_id])
-        @comment = @article.comments.find(params[:id])
         @comment.destroy
         redirect_to article_path(@article)
     end
 
     private
+    def set_article
+        @article = Article.find(params[:article_id])
+    end
+
+    def set_comment
+        @comment = @article.comments.find(params[:id])
+    end
+
     def comment_params 
         params.require(:comment).permit(:commenter, :body)
     end
